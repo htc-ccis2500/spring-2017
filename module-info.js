78c79adcfc81ed428bc4d3e1f0a506d81d392d3e
mbMosman <mbmosman@users.noreply.github.com>
2017-01-23 14:27:36 -0600

    first publish

---
+++ module-info.js
@@ -0,0 +1,7 @@
+CCIS 2500 = {
+modules: [
+  { course: "CCIS 2500", title: "Getting Started", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/getting-started", sort_order: 1, description: "An introduction to the course and the tools that we'll be using." }
+],
+ prerequisites: [
+]
+};
