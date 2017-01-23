c2d06a8dd9fbb3ebcec762f31c94cbc863d6762e
mbMosman <mbmosman@users.noreply.github.com>
2017-01-23 16:06:12 -0600

    module 2

--- module-info.js
+++ module-info.js
@@ -1,6 +1,7 @@
 CCIS 2500 = {
 modules: [
-  { course: "CCIS 2500", title: "Getting Started", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/getting-started", sort_order: 1, description: "An introduction to the course and the tools that we'll be using." }
+  { course: "CCIS 2500", title: "Getting Started", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/getting-started", sort_order: 1, description: "An introduction to the course and the tools that we'll be using." },
+  { course: "CCIS 2500", title: "Intro to Xamarin", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/mobile-intro", sort_order: 1, description: "An introduction to mobile development with Xamarin." }
 ],
  prerequisites: [
 ]
