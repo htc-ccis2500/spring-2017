5d03b158528f8875292c9619648ee3450e5a830c
mbMosman <mbmosman@users.noreply.github.com>
2017-01-30 17:02:57 -0600

    updates w3

--- module-info.js
+++ module-info.js
@@ -1,7 +1,8 @@
 CCIS 2500 = {
 modules: [
   { course: "CCIS 2500", title: "Getting Started", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/getting-started", sort_order: 1, description: "An introduction to the course and the tools that we'll be using." },
-  { course: "CCIS 2500", title: "Intro to Xamarin", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/xamarin-intro", sort_order: 2, description: "An introduction to cross-platform mobile development with Xamarin." }
+  { course: "CCIS 2500", title: "Intro to Xamarin", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/xamarin-intro", sort_order: 2, description: "An introduction to cross-platform mobile development with Xamarin." },
+  { course: "CCIS 2500", title: "Xamarin.Forms App Structure", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/xamarin-basics", sort_order: 3, description: "Introduction to the basic structure of a Xamarin.Forms cross-platform mobile application." }
 ],
  prerequisites: [
 ]
