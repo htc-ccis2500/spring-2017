f7890239ad1188298ffbd6d46e86357dd58c8456
mbMosman <mbmosman@users.noreply.github.com>
2017-02-27 01:33:38 -0600

    module 5

--- module-info.js
+++ module-info.js
@@ -3,7 +3,8 @@ modules: [
   { course: "CCIS 2500", title: "Getting Started", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/getting-started", sort_order: 1, description: "An introduction to the course and the tools that we'll be using." },
   { course: "CCIS 2500", title: "Intro to Xamarin", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/xamarin-intro", sort_order: 2, description: "An introduction to cross-platform mobile development with Xamarin." },
   { course: "CCIS 2500", title: "Xamarin.Forms App Structure", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/xamarin-basics", sort_order: 3, description: "Introduction to the basic structure of a Xamarin.Forms cross-platform mobile application." },
-  { course: "CCIS 2500", title: "Events & XAML", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/xaml-ui", sort_order: 3, description: "An introduction to event handling and building user interfaces with XAML." }
+  { course: "CCIS 2500", title: "Events & XAML", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/xaml-ui", sort_order: 3, description: "An introduction to event handling and building user interfaces with XAML." },
+  { course: "CCIS 2500", title: "Styling an App", moduleUrl: "http://htc-ccis2500.github.io/spring-2017/modules/app-styles", sort_order: 5, description: "An introduction to event handling and building user interfaces with XAML." }
 ],
  prerequisites: [
 ]
